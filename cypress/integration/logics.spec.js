394b269a3ce7ceddc3fb2a186251816b473c448e
Lucas de Morais <lucasmoraismt@gmail.com>
2022-01-06 15:42:24 -0300

    Feature: Checks if border changes when another option is selected

--- cypress/integration/logics.spec.js
+++ cypress/integration/logics.spec.js
@@ -1,5 +1,6 @@
 /// <reference types='cypress-xpath' />
 import xpath from 'cypress-xpath';
+import findInMatrix from '../helpers/findInMatrix';
 import findSimilarity from '../helpers/findSimilarity';
 
 beforeEach(() => {
@@ -69,6 +70,61 @@ describe('Layout: Itens e Botão habilitados', () => {
           this.afterColors.asHexMatrix.flat()
         )
       ).to.equal(0);
+    });
+  });
+});
+
+describe('Lógica: Seleção do combo', () => {
+  it('Desmarcando ao selecionar outro item', () => {
+    cy.xpath(
+      "//*[@data-identifier='dishes']//*[@data-identifier='food-option']"
+    )
+      .first()
+      .as('firstDish');
+    cy.analyseElement('@firstDish').as('firstBeforeClick');
+    cy.wait(0);
+
+    cy.get('@firstDish').click();
+    cy.analyseElement('@firstDish').as('firstAfterClick');
+    cy.wait(0).then(function () {
+      expect(
+        findSimilarity(
+          this.firstBeforeClick.asHexMatrix.flat(),
+          this.firstAfterClick.asHexMatrix.flat()
+        )
+      ).to.equal(0);
+    });
+
+    cy.xpath(
+      "//*[@data-identifier='dishes']//*[@data-identifier='food-option']"
+    )
+      .eq(1)
+      .as('secondDish');
+    cy.analyseElement('@secondDish').as('secondBeforeClick');
+    cy.wait(0);
+
+    cy.get('@secondDish').click();
+    cy.analyseElement('@secondDish').as('secondAfterClick');
+    cy.wait(0).then(function () {
+      expect(
+        findSimilarity(
+          this.secondBeforeClick.asHexMatrix.flat(),
+          this.secondAfterClick.asHexMatrix.flat()
+        )
+      ).to.equal(0);
+    });
+
+    cy.analyseElement('@firstDish').as('firstAfterChange');
+    cy.wait(0).then(function () {
+      expect(
+        findSimilarity(
+          this.firstAfterChange.asHexMatrix.flat(),
+          this.firstAfterClick.asHexMatrix.flat()
+        )
+      ).to.equal(0);
+      expect(
+        findInMatrix(this.secondAfterClick.asHexMatrix, '#32b72f')
+      ).to.equal('#32b72f');
     });
   });
 });
