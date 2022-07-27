1a62d5f5cceec95cd791933458ac75d1317110de
Lucas de Morais <lucasmoraismt@gmail.com>
2022-07-27 14:16:21 -0300

    fix: removing whitespaces from messageMatch check

--- cypress/helpers/messageMatch.js
+++ cypress/helpers/messageMatch.js
@@ -2,19 +2,15 @@ export default function messageMatch(url, data, checkFormat) {
   let match = 0;
   let messageRegex = /Olá,gostariadefazeropedido:-Prato:(([A-zÀ-ÿ])+?)-Bebida:(([A-zÀ-ÿ])+?)-Sobremesa:(([A-zÀ-ÿ])+?)Total:R\$[0-9]*\,[0-9]+/;
   let decodedURL = decodeURIComponent(url);
-  decodedURL = decodedURL.replace(/\s/g, ' ');
-  decodedURL = decodedURL.replace(/\n/g, ' ');
+  decodedURL = decodedURL.replace(/\s/g, '');
+  decodedURL = decodedURL.replace(' ', '');
 
-  let dish = data.dishTitle;
-  let drink = data.drinkTitle;
-  let dessert = data.dessertTitle;
+  let dish = data.dishTitle.replace(/\s/g, '');
+  let drink = data.drinkTitle.replace(/\s/g, '');
+  let dessert = data.dessertTitle.replace(/\s/g, '');
   let total = data.totalPrice.toFixed(2);
 
   if (!!checkFormat) {
-    decodedURL = decodedURL.replace(/\s/g, '');
-    decodedURL = decodedURL.replace(/\n/g, '');
-    decodedURL = decodedURL.replace(' ', '');
-
     if (messageRegex.test(decodedURL)) return 2;
   }
 
