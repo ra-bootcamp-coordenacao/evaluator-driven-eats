b6ba183432fba986b464b44292b8e84204633bc8
Lucas de Morais <lucasmoraismt@gmail.com>
2022-07-27 13:26:52 -0300

    fix: fixed messageMatch function

--- cypress/helpers/messageMatch.js
+++ cypress/helpers/messageMatch.js
@@ -1,19 +1,15 @@
 export default function messageMatch(url, data, checkFormat) {
   let match = 0;
   let messageRegex = /Olá%2Cgostariadefazeropedido%3A\sPrato%3A(([A-zÀ-ÿ])+?)\sBebida%3A(([A-zÀ-ÿ])+?)\sSobremesa%3A(([A-zÀ-ÿ])+?)\sTotal%3AR%24[0-9]*\.[0-9]+/;
-  let decodedURL = decodeURI(url);
+  let decodedURL = decodeURIComponent(url);
+  decodedURL = decodedURL.replace(/\s/g, ' ');
+  decodedURL = decodedURL.replace(/\n/g, ' ');
 
-  let dish = data.dishTitle.replace(/\s/g, '');
-  let drink = data.drinkTitle.replace(/\s/g, '');
-  let dessert = data.dessertTitle.replace(/\s/g, '');
+  let dish = data.dishTitle;
+  let drink = data.drinkTitle;
+  let dessert = data.dessertTitle;
+  let total = data.totalPrice.toFixed(2);
 
-  while(decodedURL.includes('+')) {
-    decodedURL = decodedURL.replace('+', '');
-  }
-  while (decodedURL.includes('-')) {
-    decodedURL = decodedURL.replace('-', '');
-  }
-  console.log(decodedURL, dish, drink, dessert, data.totalPrice);
   if (!!checkFormat) {
     if (messageRegex.test(decodedURL)) return 2;
   }
@@ -22,7 +18,7 @@ export default function messageMatch(url, data, checkFormat) {
     decodedURL.includes(dish) &&
     decodedURL.includes(drink) &&
     decodedURL.includes(dessert) &&
-    decodedURL.includes(data.totalPrice.toFixed(2))
+    decodedURL.includes(total.replace('.', ','))
   ) {
     match = 1;
   }
