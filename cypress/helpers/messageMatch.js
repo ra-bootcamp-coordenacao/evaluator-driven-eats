133a980f2b4f1c73692d800b589acd936086f9ff
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-28 15:27:54 -0300

    feat: meaningful messages for message match

--- cypress/helpers/messageMatch.js
+++ cypress/helpers/messageMatch.js
@@ -25,6 +25,11 @@ export default function messageMatch(url, data, checkFormat) {
     decodedText.replace('.', ',').includes(total.replace('.', ','))
   ) {
     match = 1;
+  } else {
+    if (!decodedText.includes(dish)) throw new Error(`O prato "${data.dishTitle}" não foi encontrado na mensagem a ser enviada via WhatsApp! Verifique se possui algum erro de digitação, espaçamento ou letras maiúsculas e minúsculas. Lembrando que o nome que aparece no elemento clicado deve ser exatamente igual ao nome enviado na mensagem.`);
+    if (!decodedText.includes(drink)) throw new Error(`A bebida "${data.drinkTitle}" não foi encontrada na mensagem a ser enviada via WhatsApp! Verifique se possui algum erro de digitação, espaçamento ou letras maiúsculas e minúsculas. Lembrando que o nome que aparece no elemento clicado deve ser exatamente igual ao nome enviado na mensagem.`);
+    if (!decodedText.includes(dessert)) throw new Error(`A sobremesa "${data.dessetTitle}" não foi encontrada na mensagem a ser enviada via WhatsApp! Verifique se possui algum erro de digitação, espaçamento ou letras maiúsculas e minúsculas. Lembrando que o nome que aparece no elemento clicado deve ser exatamente igual ao nome enviado na mensagem.`);
+    if (!decodedText.replace('.', ',').includes(total.replace('.', ','))) throw new Error(`O preço correto não foi encontrado na mensagem a ser enviada via WhatsApp!`);
   }
 
   return match;
