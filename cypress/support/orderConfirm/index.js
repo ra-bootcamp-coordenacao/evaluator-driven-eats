d20dc13d984a201c20640346f34a3b85ab6927f6
Lucas de Morais <lucasmoraismt@gmail.com>
2022-08-03 15:56:31 -0300

    fix: adding getExistingElement to orderConfirm

--- cypress/support/orderConfirm/index.js
+++ cypress/support/orderConfirm/index.js
@@ -2,13 +2,13 @@ function orderConfirm() {
   cy.window().then((win) => {
     cy.stub(win, 'prompt').returns('Test');
     cy.stub(win, 'open').callsFake(url => win.location.href = url)
-    cy.get('body').contains(/^fechar pedido$/i).first().click();
+    cy.getExistingElement([{text: 'fechar pedido'}, {text: 'fechar o pedido'}, {text: 'fazer pedido'}, {text: 'fazer o pedido'}]).click();
     cy.wait(0);
 
     cy.runIfElementExists(
       { xpath: "//*[@data-identifier='confirmation-dialog']" },
       () => {
-        cy.get('body').contains(/^tudo certo$/i).first().click();
+        cy.getExistingElement([{text: 'tudo certo'}, {text: 'pode pedir'}]).click();
       }
     );
   });
