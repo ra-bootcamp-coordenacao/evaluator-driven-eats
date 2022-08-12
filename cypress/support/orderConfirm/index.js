cae5a8ce1a1ecb5cde663f60fcda90a5797fa384
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-08-12 12:18:51 -0300

    fix: project called reload after open and messed up

--- cypress/support/orderConfirm/index.js
+++ cypress/support/orderConfirm/index.js
@@ -1,7 +1,9 @@
 function orderConfirm() {
   cy.window().then((win) => {
     cy.stub(win, 'prompt').returns('p');
-    cy.stub(win, 'open').callsFake(url => win.location.href = url)
+    cy.stub(win, 'open').callsFake(url => {
+      setTimeout(() => win.location.href = url, 0);
+    });
     cy.getExistingElement([{text: 'fechar pedido'}, {text: 'fechar o pedido'}, {text: 'fazer pedido'}, {text: 'fazer o pedido'}, { text: 'confirme' }, { text: 'finalizar' }, { text: 'finalize'}]).click();
     cy.wait(0);
 
